452e9e4464beb48660f198054a443eddb1122118
Park, Albert <albert.park@cbsinteractive.com>
2014-12-02 21:24:20 -0800

    Fixed globalVarReading

---
+++ fcat_1.0/src/main/java/com/cbsi/tests/util/GlobalVar.java
@@ -0,0 +1,47 @@
+package com.cbsi.tests.util;
+
+import java.io.IOException;
+
+public class GlobalVar {
+	public static String BFPId = "";
+	public static String BFPPw= "";
+	
+	public static String LocalId ="";
+	public static String LocalPw= "";
+	
+	public static String stageServer = "",
+			   devServer = "",
+			   BFPServer = ""; //need to login using their credentials.
+
+	public static String embedPath ="manageCatalog/embed-test";
+	
+	
+
+	public void setBFPId(String BFPID){
+		this.BFPId = BFPID;
+	}
+	
+	public void  setBFPPw(String BFPPw){
+		this.BFPPw = BFPPw;
+	}
+
+	public void setLocalID(String LocalID){
+		this.LocalId = LocalID;
+	}
+	
+	public void setLocalPw(String LocalPw){
+		this.LocalPw = LocalPw;
+	}
+	
+	public void setStageServer(String stageServer){
+		this.stageServer = stageServer;
+	}
+	
+	public void setDevServer(String devServer){
+		this.devServer = devServer;
+	}
+	
+	public void setBFPServer(String BFPServer){
+		this.BFPServer = BFPServer;
+	}
+}

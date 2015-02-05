0b834bde71ff0ee75f505873cfef27db664c4fde
Park, Albert <albert.park@cbsinteractive.com>
2015-02-05 15:01:22 -0800

    global var class

---
+++ fcat_1.0/src/test/java/com/cbsi/tests/Foundation/ActionsElement.java
@@ -0,0 +1,7 @@
+package com.cbsi.tests.Foundation;
+
+public class ActionsElement {
+	public final static String MAP = "map";
+	public final static String EDIT = "edit";
+	public final static String DELETE = "delete";
+}

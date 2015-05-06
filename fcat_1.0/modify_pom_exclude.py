580aad0ca48371a85378881985331f47e71dd6db
alpark <albert.park@cbsinteractive.com>
2015-05-06 15:38:42 -0700

    Added test helper for prod to edit pom before test run.

---
+++ fcat_1.0/modify_pom_exclude.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+testToExclude=''
+lines=''
+
+def read_file(pathToPom=""):
+    global lines
+    with open("pom 2.xml") as f:
+        lines=f.readlines()
+
+def add_string():
+    global lines 
+    global testToExclude
+    new_lines=''
+    endOfIncludes = False
+    startOfIncludes = False
+    for line in lines:
+        if '<excludes>' in line:
+            startOfIncludes=True
+              
+        elif '</excludes>' in line:
+            endOfIncludes=True
+            line='<excludes><exclude>' + testToExclude +'</exclude></excludes>'
+        
+        if startOfIncludes==True and endOfIncludes==False:
+            line =""
+        new_lines += line
+            
+    lines= new_lines
+    
+def write_file(pathToPom=""):
+    global lines
+    file=open(pathToPom + "pom.xml",'w')
+    file.write(lines)
+    file.close()
+
+def main(pathToPom=""):
+    read_file(pathToPom)
+    add_string()
+    write_file(pathToPom)
+
+for arg in sys.argv:
+    if arg != __file__:
+        testToExclude = arg
+
+        print('modifying ' + arg)
+        print('textToExclude:'+testToExclude)
+        main()

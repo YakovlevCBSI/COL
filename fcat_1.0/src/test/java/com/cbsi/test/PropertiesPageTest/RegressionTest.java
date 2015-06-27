4d1c9075ae1d39d6cc21e09c53fff0846706acd1
alpark <albert.park@cbsinteractive.com>
2015-06-26 17:31:15 -0700

    got rid of dummy test.

--- fcat_1.0/src/test/java/com/cbsi/test/PropertiesPageTest/RegressionTest.java
+++ fcat_1.0/src/test/java/com/cbsi/test/PropertiesPageTest/RegressionTest.java
@@ -2,7 +2,12 @@ package com.cbsi.test.PropertiesPageTest;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import com.cbsi.tests.Foundation.AllBaseTest;
@@ -18,33 +23,20 @@ public class RegressionTest extends AllBaseTest{
 	/**
 	 * Test this tmrwww to pass or fail.
 	 */
-	@Test
-	public void duplicateNameShowsCorrectErrorMessage(){
-		String catalogTitle = getExistingCatalogName();
-		
-		CatalogsPage catalogsPage= PageFactory.initElements(driver, CatalogsPage.class);
-		AddCatalogPage addCatalogPage = catalogsPage.goToAddCatalog();
-		addCatalogPage.fillInName(catalogTitle);
-		
-		assertTrue(addCatalogPage.fieldValidationErrorIsDisplayed());
-	
-	}
-	
+
 	@Test
 	public void isDefaultCatalogDisplayed(){
-		CatalogsPage catalogsPage = PageFactory.initElements(driver, CatalogsPage.class);
 		catalogsPage.setMyCatalogToManualCatalog();
 		AddCatalogPage addCatalogPage = catalogsPage.clickEdit();
 		CatalogsPage defaultCatalogPage = addCatalogPage.clickSetAsDefault().clickSave();
 		assertTrue(defaultCatalogPage.setMyCatalogToManualCatalog().isDefaultCatalog());
 		
 	}
-
 	
 	public String getExistingCatalogName(){
 		CatalogsPage catalogsPage = PageFactory.initElements(driver, CatalogsPage.class);
 		catalogsPage.setMyCatalogToManualCatalog();
-		return catalogsPage.myCatalog.getText();
+		return catalogsPage.myCatalog.getText();  
 	}
 	
 

3f52dd67fda3918875c3a34375d426f62909a10e
alpark <albert.park@cbsinteractive.com>
2015-06-30 16:11:29 -0700

    isGrid=true

--- fcat_1.0/src/main/java/com/cbsi/tests/util/GlobalVar.java
+++ fcat_1.0/src/main/java/com/cbsi/tests/util/GlobalVar.java
@@ -2,7 +2,7 @@ package com.cbsi.tests.util;
 
 public class GlobalVar {
 	public static final String JENKINS = "jenkins";
-	public static final boolean isGrid = false;
+	public static final boolean isGrid = true;
 	
 	public static String BFPId = "";
 	public static String BFPPw= "";

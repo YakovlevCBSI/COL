91b115baf6a92429787e65ea6d2ab044a36bec08
alpark <albert.park@cbsinteractive.com>
2015-07-17 14:50:45 -0700

    Adding xml testcases.

---
+++ fcat_1.0/src/test/java/com/cbsi/test/ProductsCatalogPageTest/XMLRegressionTest.java
@@ -0,0 +1,116 @@
+package com.cbsi.test.ProductsCatalogPageTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+import org.junit.runners.MethodSorters;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.cbsi.tests.Foundation.FormBaseTest;
+import com.cbsi.tests.PageObjects.CatalogsPage;
+import com.cbsi.tests.PageObjects.DetailsPage;
+import com.cbsi.tests.PageObjects.ProductsCatalogPage;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class XMLRegressionTest extends FormBaseTest{
+	private String ftpUrl = "ftp://C06515:iERUr09w0kN@janus.cnetdata.com/download/ContentCastExport";
+	private static List<String> partyCodes = new ArrayList<String>();
+
+	@Rule
+	public ExpectedException expect = ExpectedException.none();
+	
+	public XMLRegressionTest(String URL, String browser){
+		super(URL, browser);
+	}
+	
+	@Test
+	public void xmltest1_getLenovoFtpFiles() throws InterruptedException{
+		driver.get(ftpUrl);
+		
+		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h1#header")));
+
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		
+		List<WebElement> catalogs = driver.findElements(By.cssSelector("td a.icon.file"));
+		for(WebElement e: catalogs){
+			System.out.println("name: " + e.getText());
+			partyCodes.add("bfp-"+e.getText().split("_")[0]);
+		}
+		
+		//shuffle filenames
+		Collections.shuffle(partyCodes, new Random(System.nanoTime()));
+	}
+	
+	@Test
+	public void xmltest2_EditTest(){
+		 
+		for(int i=0; i<partyCodes.size()/5; i++){
+			System.out.println("filename under test: " + partyCodes.get(i));
+			ProductsCatalogPage xmlProductCatalogPage = navigateToXmlCatalog(partyCodes.get(i));
+			xmlProductCatalogPage.clickEdit().clickSave();
+			
+			assertTrue(hasNoError());
+			catalogsPage = xmlProductCatalogPage.clickReturnToList();
+		}
+	}
+	
+	@Test
+	public void xmltest3_AddProductIsDisabled(){
+		for(int i=0; i<partyCodes.size()/5; i++){
+			ProductsCatalogPage xmlProductCatalogPage = navigateToXmlCatalog(partyCodes.get(i));
+			
+			expect.expectMessage("InvocationTargetException");
+			xmlProductCatalogPage.clickAddProduct();
+			
+			catalogsPage = xmlProductCatalogPage.clickReturnToList();
+		}
+	}
+	
+	
+	@Test
+	public void xmltest4_DetailsPageStatusSucessToday(){
+		for(int i=0; i<partyCodes.size()/5; i++){
+			ProductsCatalogPage xmlProductCatalogPage = navigateToXmlCatalog(partyCodes.get(i));
+			DetailsPage detailsPage = xmlProductCatalogPage.clickShowDetails();
+			String status = detailsPage.getStatus();
+			String modifiedDate = detailsPage.getModified().split("\\s+")[0].replaceAll("‑", "");
+			String today =  new SimpleDateFormat("yyyyMMdd").format(new Date());
+	
+			assertEquals("DONE", status);
+			System.out.println(modifiedDate);
+			System.out.println(today);
+			assertEquals(modifiedDate + " / " + today, today, modifiedDate);
+			
+			catalogsPage = detailsPage.clickReturnToList();
+		}
+	}
+	
+	public ProductsCatalogPage navigateToXmlCatalog(String partyCode){
+		CatalogsPage catalogsPageLenovo = catalogsPage.clickPartyChooserIcon().searchCode(partyCode).pickFromResult();
+		ProductsCatalogPage xmlProductCatalogPage = catalogsPageLenovo.setMyCatalog("Lenovo").goToCatalogWithSomeNumberOfProducts(1);
+		
+		return xmlProductCatalogPage;
+	}
+	
+
+	
+
+}

1530c290cfd7ab8779e5e0339dc8c079f8495024
alpark <albert.park@cbsinteractive.com>
2015-09-30 11:33:26 -0700

    new page added.

---
+++ src/main/java/com/cbsi/col/pageobject/documents/RecycleBinPage.java
@@ -0,0 +1,27 @@
+package com.cbsi.col.pageobject.documents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.cbsi.col.pageobject.home.ColBasePage;
+
+public class RecycleBinPage extends ColBasePage{
+
+	public RecycleBinPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+		waitForTextToBeVisible("Recycle Bin", "h1");
+	}
+	
+	public RecycleBinPage restoreByDocNumber(long docNumber){
+		findDataRowByName(docNumber+"", 2).findElement(By.xpath("td/[14]/input")).click();
+		forceWait(1000);
+		return PageFactory.initElements(driver, RecycleBinPage.class);
+	}
+	
+	public boolean hasDoc(long docNumber){
+		return findDataRowByName(docNumber+"", 2)==null?false:true;
+	}
+
+}

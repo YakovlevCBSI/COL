af54b1c61163289c2e2cb3d703f82ab5f4f0f06c
alpark <albert.park@cbsinteractive.com>
2015-10-12 09:59:26 -0700

    login updated

--- src/main/java/com/cbsi/col/test/util/LoginProperty.java
+++ src/main/java/com/cbsi/col/test/util/LoginProperty.java
@@ -4,6 +4,11 @@ public class LoginProperty {
 //	public static final String testUser = "shefali.ayachit@cbsinteractive.com";
 //	public static final String testUser = "albert.park@cbsinteractive.com";
 	public static final String testUser = "cbsiqa@gmail.com";
+
+	public static final String testUser1 = "cbsiqa@gmail.com";
+	public static final String testUser2 = "cbsiqa0@gmail.com";
+	public static final String testUser3 = "cbsiqa1@gmail.com";
+
 	public static final String testPassword = "Password1";
 
 

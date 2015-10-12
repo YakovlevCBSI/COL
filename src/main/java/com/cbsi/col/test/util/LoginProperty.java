c3a034d25cb23638e21db6754fbee793a0edff95
alpark <albert.park@cbsinteractive.com>
2015-10-12 10:26:52 -0700

    change user

--- src/main/java/com/cbsi/col/test/util/LoginProperty.java
+++ src/main/java/com/cbsi/col/test/util/LoginProperty.java
@@ -2,8 +2,8 @@ package com.cbsi.col.test.util;
 
 public class LoginProperty {
 //	public static final String testUser = "shefali.ayachit@cbsinteractive.com";
-//	public static final String testUser = "albert.park@cbsinteractive.com";
-	public static final String testUser = "cbsiqa@gmail.com";
+	public static final String testUser = "albert.park@cbsinteractive.com";
+//	public static final String testUser = "cbsiqa@gmail.com";
 
 	public static final String testUser1 = "cbsiqa@gmail.com";
 	public static final String testUser2 = "cbsiqa0@gmail.com";

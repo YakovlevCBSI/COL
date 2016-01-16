7e544bb4e2f23b793ec6e6535478f2be06a7f4bc
alpark <albert.park@cbsinteractive.com>
2016-01-15 17:57:10 -0800

    checkempty.

--- src/main/java/com/cbsi/col/test/util/GlobalProperty.java
+++ src/main/java/com/cbsi/col/test/util/GlobalProperty.java
@@ -5,8 +5,9 @@ public class GlobalProperty {
 	public static String USERNAME = System.getProperty("username");
 	public static String GRID = System.getProperty("grid");
 	
-	public static boolean isProd = System.getProperty("environment")==null?false:true;
-	
+	public static boolean isProd = System.getProperty("environment").isEmpty()?false:true;
+//	public static boolean isProd = true;
+
 	public static String getProperty(String propertyValue, String defaultValue){
 		if(propertyValue == null){
 			return defaultValue;
